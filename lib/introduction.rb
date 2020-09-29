9ede6efab575af11b69d0cb9f96c8de602847308
Guillermo Chang <e.guillermo@rocketmail.com>
2020-09-29 02:21:39 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,3 +1,6 @@
+def introduction(name)
+  puts "Hi, my name is #{name}."
+end
 
 def introduction_with_language(name=guillermo, language=ruby)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
